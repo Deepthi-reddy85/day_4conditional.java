13c0ff871ddbf4d2077924989b750e8d2f52f0a6
Deepthi-reddy85 <2311cs010636@mallareddyuniversity.ac>
2025-01-06 21:43:00 +0530

    changed mode

---
+++ main.java/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x=10,y=20;
+        System.out.println(x>y);
+        System.out.println(x<y);
+        System.out.println(x=y);
+    
+    }
+}
---
+++ main.java/src/session4.java
@@ -0,0 +1,5 @@
+public class session4 {
+ public static void main(String[] args) {
+     
+ }}   
+}
