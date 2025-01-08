01114805d16ac7d333a2ffac47b4adcd0ba77394
Deepthi-reddy85 <2311cs010636@mallareddyuniversity.ac>
2025-01-08 17:54:24 +0530

    changed mode

---
+++ main.java/src/Day_6_array.java
@@ -0,0 +1,9 @@
+public class Day_6_array {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        
+
+
+    }
+    
+}
---
+++ main.java/src/Day_6_star.java
@@ -0,0 +1,10 @@
+public class Day_6_star {
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++) {  //outerloops:rows
+            for(int j=0;j<3;j++){
+                System.out.print("*");
+            }
+                System.out.println();
+    }
+}  
+}
